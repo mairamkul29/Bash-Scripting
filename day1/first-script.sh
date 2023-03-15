bc816a527a04d1d82454fac2127e2b0c2920fc3d
mairamkul29 <mairamkul29@gmail.com>
2023-03-15 00:55:44 +0000

    modified first-script.sh file

--- day1/first-script.sh
+++ day1/first-script.sh
@@ -4,6 +4,6 @@ echo "this script name is $0" ## $0 represents script itself by presenting the n
 
 echo "Hello there $1 $2 $3 $4" ## any shell script by default has 9 arguments ($1..$9)
 
-echo "This is the list of all arguments given by the user: $0"
+echo "This is the list of all arguments given by the user: $@"
 
 echo "This script is running with this PID: $$"
