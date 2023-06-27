fe2305ef589c46404f4341c198ef0259d23aeaee
Mairamkul Ramazanova <126841328+mairamkul29@users.noreply.github.com>
2023-06-27 10:03:44 -0500

    Delete slack.sh

--- slack.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-
-
-curl -X POST -H "Content-Type: application/json" \
-    -H "Access-Control-Allow-Headers: Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token" \
-    -H "Access-Control-Allow-Origin*: *" \
-    -d '{"name": "mairamkul29", "email": "mairamkul29@gmail.com", "service": "bootcamp", "message": "Hey there, this is my api call to aKumo"}'\
-   
